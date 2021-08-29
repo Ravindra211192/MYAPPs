34675a5e893b48c1910c274d810055094a1fab60
Chand <R.Chand@rehm-online.de>
2021-08-29 19:43:52 +0200

    Adding Test program for Delegates in C#

---
+++ Delegates/DownloadHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Delegates
+{   
+    public class FileEventArgs: EventArgs
+    {
+        public File File { get; set; }
+    }
+    public class DownloadHelper
+    {
+        //Step 1 - create a delegate
+        //public delegate void FileDownloadedEventHandler(object source, EventArgs args);
+        
+        //Step 2 - create an event based on that delegate
+        //public event FileDownloadedEventHandler FileDownloaded;
+        public event EventHandler<FileEventArgs> FileDownloaded;
+
+        //Step 3 - raise the event
+        protected virtual void onFileDownloaded(File file)
+        {
+            if(FileDownloaded != null)
+            {
+                FileDownloaded(this,new FileEventArgs() { File = file});
+            }
+        }
+        public void Download(File file)
+        {
+            Console.WriteLine("Downloading file ........");
+            Thread.Sleep(4000);
+
+            //Step 3.1
+            onFileDownloaded(file);
+        }
+
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate string GetTextDelegate(string aname);
+        public delegate double GetMultipleDelegate(double aFirstNr, double aSecondNr);
+        static void Main(string[] args)
+        {   
+            //Anonymous(Unknown) function
+            GetTextDelegate agetTextDelgate = delegate (string aname)
+            {
+                return "Hello, " + aname + "Chand";
+            };
+            //expression lambda
+            GetTextDelegate agetDkDelegate = (string aname) => { return "Hello" +aname; };
+            //body lambda
+            GetTextDelegate agetTestLambdaBody = (string aname) => 
+            { 
+                Console.WriteLine("I am inside the body lambda");
+                return "Bye Lambda" + aname;
+            };
+            Console.WriteLine(agetTextDelgate("Ravindra "));
+            Console.WriteLine(agetDkDelegate(" DK!"));
+            Console.WriteLine(agetTestLambdaBody("Body lambda test is done!"));
+
+            GetMultipleDelegate aGetMultipleDelegate = (aFirstNr, aSecondNr) => aFirstNr * aSecondNr;
+            DisplayMultipleDelegate(aGetMultipleDelegate);
+
+            GetMultipleDelegate aGetAdditionDelegate = (aFirstN, aSecondN) => aFirstN + aSecondN;
+            DisplayMultipleDelegate(aGetAdditionDelegate);
+
+            var file = new File() { Titel = "File 1" };
+            var aDownloadHelper = new DownloadHelper(); //Publisher
+           
+            var aUnpackService = new UnpackService(); //Receiver
+            //Here we don't call method aUnpackService() but just adds the pointer to it
+            aDownloadHelper.FileDownloaded += aUnpackService.OnFileDownloaded;
+
+            var aNotificationService = new NotificationService();
+            aDownloadHelper.FileDownloaded += aNotificationService.OnFileDownloaded;
+
+            aDownloadHelper.Download(file);
+
+        }
+
+        static void DisplayMultipleDelegate(GetMultipleDelegate aGetMultipleDelegate)
+        {
+            Console.WriteLine(aGetMultipleDelegate(12.0,3.0));
+        }
+    }
+
+    public class UnpackService
+    {
+        public void OnFileDownloaded(object source, FileEventArgs e)
+        {
+            Console.WriteLine("UnpackerService: Unpacking the file ....." + e.File.Titel);
+        }
+    }
+
+    public class NotificationService
+    {
+        public void OnFileDownloaded(object source, FileEventArgs a)
+        {
+            Console.WriteLine("NotificationService: Receiving the notifications ....."+a.File.Titel);
+        }
+    }
+}
